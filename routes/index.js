368d3fc3d3912fef56b57f14c43a2cbaae3d09d0
Hamdan Salih <hamdansalihpc@gmail.com>
2023-02-16 21:51:51 +0530

    Display messages and add message form

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req,res,next){
+  res.render('form' ,)
+})
+
+module.exports = router;
