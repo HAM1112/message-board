d5e16eb102f3ba4aa413069e816a328895728c72
Hamdan Salih <hamdansalihpc@gmail.com>
2023-02-17 20:17:31 +0530

    form design

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
+    text: "The monkey juggled the coconuts with ease, impressing the onlookers",
+    user: "Yasmin",
     added: new Date()
   },
   {
-    text: "Hello World!",
+    text: "As the sun set behind the mountains, the sky was painted with shades of pink and orange, creating a stunning display of natural beauty",
     user: "Charles",
     added: new Date()
   }
@@ -25,7 +25,7 @@ router.get('/new', function(req,res,next){
 
 router.post('/new' ,function(req,res,next){
   if(req.body.user === '' && req.body.text === ''){
-    res.render('form',{title: 'Add MSG',error: 'input cannot be empty'})
+    res.render('form',{title: 'Add MSG',error: 'Inputs cannot be empty'})
   }else{
     messages.push({text: req.body.text , user : req.body.user, added : new Date() })
     res.redirect('/')
