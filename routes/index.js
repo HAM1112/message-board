893b3aa0061dfab69eefa49a704e00cce9e6b29c
Hamdan Salih <hamdansalihpc@gmail.com>
2023-02-17 09:29:57 +0530

    completed redirecting

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/new', function(req,res,next){
-  res.render('form' ,)
+  res.render('form' ,{title:"Add MSG"})
 })
+
+router.post('/new' ,function(req,res,next){
+  if(req.body.user === '' && req.body.text === ''){
+    res.render('form',{title: 'Add MSG',error: 'input cannot be empty'})
+  }else{
+    messages.push({text: req.body.text , user : req.body.user, added : new Date() })
+    res.redirect('/')
+  }
+} )
 
 module.exports = router;
